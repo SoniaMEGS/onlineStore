5392cde105e33487879dd76eafb3638533cb1ea8
SoniaMEGS <megs.sonia@gmail.com>
2024-04-29 14:58:15 -0700

    Installing React with Vite and TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-purple-300">Holi</div>
+    </>
+  );
+}
+
+export default App;
