1761d883991ac877ef7eb0ebbfb3fb6ac50bdf32
SoniaMEGS <megs.sonia@gmail.com>
2024-04-29 20:40:45 -0700

    Routing with React Router DOM

---
+++ src/pages/app/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import Home from "../home/Home.jsx";
+import MyAccount from "../myAccount/MyAccount.jsx";
+import MyOrders from "../myOrders/MyOrders.jsx";
+import MyOrder from "../myOrder/MyOrder.jsx";
+import SingIn from "../singIn/SingIn.jsx";
+import NotFound from "../notFound/NotFound.jsx";
+import "../../App.css";
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/myAccount", element: <MyAccount /> },
+    { path: "/myOrders", element: <MyOrders /> },
+    { path: "/myOrder", element: <MyOrder /> },
+    { path: "/singIn", element: <SingIn /> },
+    { path: "/*", element: <NotFound /> },
+  ]);
+  return routes;
+};
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
